ffde120761dad827756e698da878c9c563ff8786
pranjalkumar445 <pranjalkumar445@gmail.com>
2026-02-16 15:09:00 +0530

    first c

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <header className="fixed top-0 w-full z-50 border-b border-gray-100 bg-white">
             <div className="wrapper h-16 md:h-24 flex items-center justify-start">
                 <Link
-                    href="https://www.gdgoenkadhanbad.in/"
+                    href="https://bhatnagarinternational.org/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="flex items-center gap-3 group h-full"
